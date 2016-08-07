30c628a3b104533ba30f855254e88a32f42191ec
Aleksandr Sivakow <alexandrsivakov@me.com>
2016-08-07 17:06:21 +0300

    Fixes for working on windows

--- chat/createDb.js
+++ chat/createDb.js
@@ -1,43 +1,45 @@
 var mongoose = require('./libs/mongoose');
+mongoose.set('debug', true)
 var async = require('async');
-var User = require('./models/user').User;
+
 async.series([
     open,
     dropDatabase,
     requireModels,
-    createUsers,
-    showAllUsers
-], function(err) {
+    createUsers
+], function(err, results){
     console.log(arguments);
     mongoose.disconnect();
-    process.exit(err ? 255 : 0);
-});
+    process.exit(err ? 255: 0);
+})
 
-function open(callback) {
+function open(callback){
     mongoose.connection.on('open', callback);
 }
 
-function dropDatabase(callback) {
+function dropDatabase(callback){
     var db = mongoose.connection.db;
     db.dropDatabase(callback);
 }
 
-function requireModels(callback) {
+function requireModels(callback){
     require('./models/user');
 
-    async.each(Object.keys(mongoose.models), function(modelName, callback) {
+    async.each(Object.keys(mongoose.models), function(modelName, callback){
         mongoose.models[modelName].ensureIndexes(callback);
+    }, callback)
+}
+
+function createUsers(callback){
+    require('./models/user');
+
+    var users = [
+        {username: 'Vasya', password: '12341234'},
+        {username: 'petya', password: '12341234'},
+        {username: 'admin', password: '12341234'}
+    ];
+    async.each(users, function(userData, callback){
+        var user = new mongoose.models.User(userData);
+        user.save(callback);
     }, callback);
-}
-
-function createUsers(callback) {
-
-
-}
-
-function showAllUsers(callback) {
-    User.find({}, function(err, users) {
-        if (err) throw err;
-        console.log(users);
-    });
 }
