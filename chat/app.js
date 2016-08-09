9625c61dce3d1e5c8fedde918744641a529bf175
Aleksandr Sivakow <alexandrsivakov@me.com>
2016-08-09 20:50:56 +0300

    Chapter 38. Sessions

--- chat/app.js
+++ chat/app.js
@@ -5,10 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var HttpError = require('./error').HttpError;
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var config = require('./config');
+var session = require('express-session');
 
 var app = express();
+var mongoose = require('./libs/mongoose')
+var MongoStore = require('connect-mongo')(session);
 
 // view engine setup
 app.engine('ejs', require('ejs-locals'));
@@ -21,40 +26,60 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: config.get('session:secret'),
+  key: config.get('session:key'),
+  cookie: config.get('session:cookie'),
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  })
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+  req.session.visits_number = req.session.visits_number + 1 || 1;
+  res.send("Visits:" + req.session.visits_number);
+});
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
 
+
+
+app.use(require('./middleware/sendHttpError'));
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  if(typeof err == 'number'){
+    err = new HttpError(err);
+  }
+  if(err instanceof HttpError){
+    res.sendHttpError(err);
+  } else {
+    if (app.get('env') === 'development') {
+      express.errorHandler()(err, req, res, next);
+    } else {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: {}
+      });
+    }
+  }
+
+
 });
 
 
--- chat/routes/index.js
+++ chat/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/user').User;
+var HttpError = require('../error').HttpError;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Burn in hell, express creator' });
+  req.session.visits_number = req.session.visits_number + 1 || 1;
+  res.render('index', { title: req.session.visits_number });
+});
+
+router.get('/users', function(req, res, next){
+  User.find({}, function(err, users){
+    if(err) return next(err);
+    res.json(users);
+  })
+});
+
+router.get('/users/:id', function(req, res, next){
+  User.findById(req.params.id, function(err, user){
+    if(err) return next(err);
+    if (!user){
+      next(new HttpError(404, "User not found"));
+    }
+    res.json(user);
+  });
 });
 
 
